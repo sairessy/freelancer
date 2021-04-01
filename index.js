e883c6720faf0106e4faa7bbf9866795f228bcd7
sairessy <sairessy@gmail.com>
2021-04-01 14:08:06 +0300

    new changes

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())
 
+const port = process.env.PORT || 3000;
+
 app.listen(3000, ()=> {
   console.log('Server running on port 3000')
 })
