18ae327bb6672df80911a8ee5d28f2bf5e0051fe
sairessy <sairessy@gmail.com>
2021-04-07 00:44:23 +0300

    updating

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cookieParser())
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=> {
-  console.log('Server running on port 3000')
+app.listen(port, ()=> {
+  console.log(`Server running on port ${port}!`)
 })
 
 const db = {
